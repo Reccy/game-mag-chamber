8147d7ef8ea7e40457e9e348ca8372a335bc4844
Reccy <aaronmeaney@hotmail.com>
2016-08-26 22:04:06 +0100

    Fixed scene transition sorting layer.

---
+++ Assets/Scripts/Camera/CanvasConnector.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CanvasConnector : MonoBehaviour {
+
+	void Awake()
+    {
+        GetComponent<Canvas>().worldCamera = Camera.main;
+    }
+}
