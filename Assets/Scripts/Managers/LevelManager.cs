ae8c79e7429e2bdcad130f75ab051052baae98b0
Reccy <aaronmeaney@hotmail.com>
2016-09-17 22:48:58 +0100

    Removed Blur Bench scene from Shader Asset Pack. Began work on Level Manager.

---
+++ Assets/Scripts/Managers/LevelManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour
+{
+    //Phase management
+    public Phase[] phases; //Array of level phases
+    private int currentPhase; //Current phase of the level
+
+    private float levelElapsedTime; //Time since beginning of level
+
+    //Start the level again
+    public void StartLevel()
+    {
+        levelElapsedTime = 0;
+        currentPhase = 0;
+    }
+
+    
+}
+
+//A level phase
+[System.Serializable]
+public class Phase
+{
+    public Obstacle[] obstacles; //Array of obstacles in this phase
+    public float patternSpawnRate; //Rate at which each new pattern spawns in seconds.
+    public float nextPhaseTime; //Time in seconds until the next phase. Cumulative from last phase.
+}
+
+
+//An obstacle
+[System.Serializable]
+public class Obstacle
+{
+    public GameObject obstacleObject; //Obstacle game object
+}
