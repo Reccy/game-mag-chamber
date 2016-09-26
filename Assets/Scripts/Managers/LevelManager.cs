1d627964933747905644afc84f509083d5da5863
Reccy <aaronmeaney@hotmail.com>
2016-09-26 19:43:23 +0100

    Refactored phase dependant code. Fixed time accumulator.

--- Assets/Scripts/Managers/LevelManager.cs
+++ Assets/Scripts/Managers/LevelManager.cs
@@ -30,13 +30,13 @@ public class LevelManager : MonoBehaviour
     {
         levelStartTime = Time.time;
         levelElapsedTime = 0;
-        phaseIndex = -1;
+        phaseIndex = 0;
         currentPhase = phases[0];
         currentObstacles = 0;
         lastSpawnTime = 0;
         accumulatedPhaseTime = currentPhase.phaseDuration;
-        NextPhase();
         StartCoroutine(LevelManagerCoroutine());
+        PhaseCode(phaseIndex);
     }
 
     //Go to next phase
@@ -48,23 +48,24 @@ public class LevelManager : MonoBehaviour
             phaseIndex++;
             currentPhase = phases[phaseIndex];
             accumulatedPhaseTime += currentPhase.phaseDuration;
+            PhaseCode(phaseIndex);
+        }
+    }
 
-            //Phase dependant code execution
-            switch (phaseIndex)
-            {
-                case 0:
-                    Debug.Log("FIRST!!!!11!!");
-                    break;
-                case 1:
-                    Debug.Log("second");
-                    break;
-                case 2:
-                    Debug.Log("second (index) ;)");
-                    break;
-                case 3:
-                    Debug.Log("THIRD INDEX! :D");
-                    break;
-            }
+    //Phase dependant code execution
+    void PhaseCode(int phaseIndex)
+    {
+        switch (phaseIndex)
+        {
+            case 0:
+                Debug.Log("First");
+                break;
+            case 1:
+                Debug.Log("Second");
+                break;
+            case 2:
+                Debug.Log("Third");
+                break;
         }
     }
 
@@ -112,8 +113,7 @@ public class LevelManager : MonoBehaviour
                 NextPhase();
             }
 
-            if(HasNextPhase())
-                Debug.Log("Current Phase: " + phaseIndex + " || Current Time: " + levelElapsedTime + " || Change Time: " + accumulatedPhaseTime);
+            //Debug.Log("Current Phase: " + phaseIndex + " || Current Time: " + levelElapsedTime + " || Change Time: " + accumulatedPhaseTime);
             
             //Update UI Time
             int minutes, seconds, timeInSeconds;
