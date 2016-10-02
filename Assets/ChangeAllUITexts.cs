0aed6c9a8a3926946a6c6f3ac9eac061dcc4cd9f
Reccy <aaronmeaney@hotmail.com>
2016-10-02 22:42:03 +0100

    Added new font. Added font glow.

---
+++ Assets/ChangeAllUITexts.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEditor;
+
+[ExecuteInEditMode]
+public class ChangeAllUITexts : MonoBehaviour
+{
+    public Font replacementFont;
+
+    void Awake()
+    {
+        Text[] textArray = Object.FindObjectsOfType<Text>();
+
+        foreach(Text text in textArray)
+        {
+            text.font = replacementFont;
+        }
+    }
+}
