f37d83a3d44702386383bd9f1a3dd841eff72c17
Reccy <aaronmeaney@hotmail.com>
2016-10-23 13:05:47 +0100

    Added anticipation state to Crop Duster enemy.

--- Assets/Scripts/BulletPatterns/Enemy_CDuster.cs
+++ Assets/Scripts/BulletPatterns/Enemy_CDuster.cs
@@ -1,13 +1,25 @@
 ﻿using UnityEngine;
 using UnityExtensions.MathfExtensions;
 using System.Collections;
+using MonsterLove.StateMachine;
 
 public class Enemy_CDuster : BulletPattern {
 
+    private enum State { Anticipation, Moving };
+    private StateMachine<State> state;
+
     private float speed = 6f; //Speed of cduster
-    private float shootLimit = 1.34f; //Duration between shots
+    private float anticipationSpeed = 3f; //Speed during anticipation state
+    private float shootLimit = 0.6f; //Duration between shots
     private float originalShootLimit; //Original duration between shots
+    private float anticipationTimer = 0.5f; //Duration of anticipation state
     public GameObject bullet; //Bullet to be instantiated
+
+    void Awake()
+    {
+        state = StateMachine<State>.Initialize(this);
+        state.ChangeState(State.Anticipation);
+    }
 
     void Start()
     {
@@ -21,7 +33,34 @@ public class Enemy_CDuster : BulletPattern {
         originalShootLimit = shootLimit;
     }
 
-    void FixedUpdate()
+    void Anticipation_FixedUpdate()
+    {
+        //Slow down once object enters the game boundaries
+        if ((transform.position.x > -9.4f && transform.position.x < 9.4f) && (transform.position.y > -7.3f && transform.position.y < 7.3f))
+        {
+            if (anticipationSpeed <= 0)
+            {
+                anticipationSpeed = 0;
+                if (anticipationTimer < 0)
+                {
+                    state.ChangeState(State.Moving);
+                }
+                else
+                {
+                    anticipationTimer -= GameManager.slowMotionMultiplier * Time.deltaTime;
+                }
+            }
+            else
+            {
+                anticipationSpeed -= 0.05f * GameManager.slowMotionMultiplier;
+            }
+        }
+
+        //Move forwards
+        transform.Translate(Vector2.up * anticipationSpeed * GameManager.slowMotionMultiplier * Time.deltaTime);
+    }
+
+    void Moving_FixedUpdate()
     {
         //Move forward very quickly
         transform.Translate(Vector2.up * speed * GameManager.slowMotionMultiplier * Time.deltaTime);
