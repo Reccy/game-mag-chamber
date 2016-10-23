ef1e0b537573dc97a781583b8b463a0a25c80370
Reccy <aaronmeaney@hotmail.com>
2016-10-23 12:54:27 +0100

    Added anticipation state to Pointer enemy.

--- Assets/Scripts/BulletPatterns/Enemy_Pointer.cs
+++ Assets/Scripts/BulletPatterns/Enemy_Pointer.cs
@@ -5,20 +5,67 @@ using MonsterLove.StateMachine;
 
 public class Enemy_Pointer : BulletPattern
 {
-    private enum State { Seeking, Shooting }
+    private enum State { Anticipation, Seeking, Shooting }
     private StateMachine<State> state;
+    private float anticipationTimer = 0.5f;
+    private float anticipationSpeed;
     private float endTimer = 5f;
+    private float speed = 3;
 
     void Awake()
     {
         state = StateMachine<State>.Initialize(this);
-        state.ChangeState(State.Seeking);
+        state.ChangeState(State.Anticipation);
+    }
+
+    void Start()
+    {
+        if (Player)
+        {
+            float angleToPlayer = MathfExtensions.AngleFromTo(this.gameObject, Player);
+            transform.rotation = Quaternion.AngleAxis(angleToPlayer, Vector3.forward);
+            transform.Rotate(0, 0, transform.rotation.z + Random.Range(-5, 5));
+        }
+    }
+
+    void Anticipation_Enter()
+    {
+        anticipationSpeed = speed;
+    }
+
+    void Anticipation_FixedUpdate()
+    {
+        //Slow down once object enters the game boundaries
+        if ((transform.position.x > -9.4f && transform.position.x < 9.4f) && (transform.position.y > -7.3f && transform.position.y < 7.3f))
+        {
+            if(anticipationSpeed <= 0)
+            {
+                anticipationSpeed = 0;
+                if(anticipationTimer < 0)
+                {
+                    state.ChangeState(State.Seeking);
+                }
+                else
+                {
+                    anticipationTimer -= GameManager.slowMotionMultiplier * Time.deltaTime;
+                }
+            }
+            else
+            {
+                anticipationSpeed -= 0.05f * GameManager.slowMotionMultiplier;
+            }
+        }
+
+        //Move forwards
+        transform.Translate(Vector2.up * anticipationSpeed * GameManager.slowMotionMultiplier * Time.deltaTime);
+
+        Debug.Log("Anticipation Speed: " + anticipationSpeed + ", Anticipation Timer: " + anticipationTimer + ", Transform Position: " + transform.position);
     }
 
     void Seeking_FixedUpdate()
     {
         //Move forwards
-        transform.Translate(Vector2.up * 3 * GameManager.slowMotionMultiplier * Time.deltaTime);
+        transform.Translate(Vector2.up * speed * GameManager.slowMotionMultiplier * Time.deltaTime);
 
         //Rotate towards player
         if(Player)
